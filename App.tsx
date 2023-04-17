90d65a936d6b11babdcfaf554e8f460aac0ac37c
RafaelSanaMontevechio <rafael_sana@hotmail.com>
2023-04-17 09:34:14 -0300

    feat: :sparkles: add theme

--- App.tsx
+++ App.tsx
@@ -8,20 +8,20 @@ import {
   Roboto_400Regular,
 } from '@expo-google-fonts/roboto';
 
+import { THEME } from './src/theme';
 import { Loading } from '@components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      {/* {fontsLoaded ? <Text>Hello world</Text> : <Loading />} */}
-      <Loading />
+      {fontsLoaded ? <Text>Hello world</Text> : <Loading />}
     </NativeBaseProvider>
   );
 }
